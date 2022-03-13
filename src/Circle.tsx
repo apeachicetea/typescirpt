423ae4526b79684e8495994ff5c306210b6337dd
kimdaeyoon <eodbszla@gmail.com>
2022-03-13 12:25:01 +0900

    Add: State 타입 코드 추가

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -23,6 +23,10 @@ const Container = styled.div<IContainerProps>`
 `;
 
 function Circle({ bgColor, borderColor, text = "default text" }: ICircleProps) {
+  // useState의 타입을 < number | string > 와 같이 정의할 수 있지만
+  // useState에 default Value의 타입에 따라 타입스크립트가 같은 타입으로 오는것을 인지하기에 따로 지정은 불필요
+  // const [counter, setCounter] = useState<number | string>(1);
+  const [counter, setCounter] = useState(1);
   return (
     <>
       {/* borderColor ?? bgColor => borderColor가 undefined일떄 ?? 뒤를 채택 */}
