5d54bc307b8f4e59cf9b1c9bc9bfbc5364c173d3
kimdaeyoon <eodbszla@gmail.com>
2022-03-13 12:38:42 +0900

    Add: Form 타입 지정 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import React, { useState } from "react";
 import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <div>
       <Circle bgColor='tomato' borderColor='yellow' />
       <Circle bgColor='teal' text='Hello' />
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          value={value}
+          onChange={onChange}
+          placeholder='username'
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
