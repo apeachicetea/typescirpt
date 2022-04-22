194b8740c2843efba56b2e49ac401d0b513ac5c8
kimdaeyoon <eodbszla@gmail.com>
2022-04-22 20:47:05 +0900

    Types of TS Part One

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,39 @@
-import loadable from "@loadable/component";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+//basic syntax
+let a: number = 1;
+let b: string = "i1";
+let c: boolean = true;
+let d: boolean[] = [false];
 
-//코드 스플리팅
-const Workspace = loadable(() => import("./Workspace/index"));
+//optinal type
+const player: { name: string; age?: number } = {
+  name: "nico",
+};
+
+//type aliases
+type Player = { name: string; age?: number };
+
+const User: Player = {
+  name: "kim",
+  age: 32,
+};
+
+const Friend: Player = {
+  name: "Park",
+};
+
+//function type
+function playerMaker(name: string): Player {
+  return {
+    name,
+  };
+}
+
+const daeyoon: Player = playerMaker("daeyoon");
+daeyoon.age = 32;
+
+const playerMaker2 = (name: string): Player => ({ name });
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/workspaces/:workspace/*" element={<Workspace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>App</h1>;
 }
 export default App;
--- src/DirectMessage/index.tsx
+++
@@ -1,5 +0,0 @@
-const DirectMessage = () => {
-  return <div>로그인하신 것을 축하드려요!</div>;
-};
-
-export default DirectMessage;
