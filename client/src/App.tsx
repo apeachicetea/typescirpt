ccc4cb1363b6c4ac6d151e99261e4754eb0a4f48
kimdaeyoon <eodbszla@gmail.com>
2022-04-23 11:34:01 +0900

    Overloading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,43 @@
-//call signatures
-type Add = (a: number, b: number) => number;
+//overloading
+//오버로딩은 함수가 서로 다른 여러개의 call signatures를 가지고 있을 때 발생시킨다
+type Add = {
+  (a: number, b: number): number;
+  (a: number, b: string): number;
+};
 
-const add: Add = (a, b) => a + b;
+const add: Add = (a, b) => {
+  if (typeof b === "string") return a;
+  return a + b;
+};
+
+type Add1 = {
+  (a: number, b: number): number;
+  (a: number, b: number, c: number): number;
+};
+
+const add1: Add1 = (a, b, c?: number) => {
+  if (c) return a + b + c;
+  return a + b;
+};
+
+//next.js에서 Router.push사용시 overloading 사용 예시
+type Config = {
+  path: string;
+  state: object;
+};
+
+type Push = {
+  (path: string): void;
+  (config: Config): void;
+};
+
+const push: Push = (config) => {
+  if (typeof config === "string") {
+    console.log(config);
+  } else {
+    console.log(config.path);
+  }
+};
 
 function App() {
   return <h1>App</h1>;
